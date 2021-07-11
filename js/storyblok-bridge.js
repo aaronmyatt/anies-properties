ec08190711309021dd9b6272ff0b6b034e6bb22f
aaronmyatt <aaronmyatt@gmail.com>
2021-07-11 19:40:08 +0800

    c144c87

---
+++ js/storyblok-bridge.js
@@ -0,0 +1,37 @@
+function ready(callback) {
+  if(document.readyState !== 'loading'){
+    callback()
+  } else {
+    document.addEventListener('DOMContentLoaded', callback)
+  }
+}
+
+function loadBridge(callback) {
+  existingScript = document.getElementById("storyblokBridge")
+  if(existingScript){
+    callback()
+  } else {
+    script = document.createElement("script")
+    script.src = "https://app.storyblok.com/f/storyblok-v2-latest.js"
+    script.id = "storyblokBridge"
+    document.body.appendChild(script)
+    script.onload = function(){
+      callback()
+    }
+  }
+}
+
+ready(loadBridge(function(){
+  console.log('Preparing Bridge')
+
+  storyblokInstance = new window.StoryblokBridge()
+
+  storyblokInstance.on(['published', 'change'], function(event){
+    console.log(event)
+  })
+
+  storyblokInstance.on('input', function(event){
+    console.log(event)
+    Alpine.store('page', event.story.content)
+  })
+}))
